52343b73434f18e6beb25b6e5c23a98e4cf241d4
Robert Tiemann <r.tiemann@ta-hifi.com>
2016-01-21 15:21:58 +0100

    Fix copyright headers.
    Change-Id: Ie7d01f7b4995e6b84ea1c7162a01a3905a59d03d

--- tests/test_inifile.cc
+++ tests/test_inifile.cc
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015, 2016  T+A elektroakustik GmbH & Co. KG
  *
- * This file is part of DCPD.
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
  *
- * DCPD is free software: you can redistribute it and/or modify
+ * StrBoWare is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 3 as
  * published by the Free Software Foundation.
  *
- * DCPD is distributed in the hope that it will be useful,
+ * StrBoWare is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with DCPD.  If not, see <http://www.gnu.org/licenses/>.
+ * along with StrBoWare.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #if HAVE_CONFIG_H

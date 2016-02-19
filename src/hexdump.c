2475eb119eb1f8347cac1fd251d77aef21093293
Robert Tiemann <r.tiemann@ta-hifi.com>
2016-02-19 15:35:48 +0100

    Add generic hexdump function.
    Change-Id: I3779ca181f085e86db21c047d2925d7ff9f2046b

---
+++ src/hexdump.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
+ *
+ * StrBoWare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * StrBoWare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StrBoWare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+
+#include "hexdump.h"
+#include "messages.h"
+
+void hexdump_to_log(const uint8_t *const buffer, size_t buffer_length,
+                    const char *what)
+{
+    msg_info("%s (%zu bytes):", what, buffer_length);
+
+    if(buffer == NULL || buffer_length == 0)
+        return;
+
+    static const size_t num_of_columns = 16;
+    char log_buffer[1024];
+    size_t out_pos = 0;
+
+    for(size_t i = 0; i < buffer_length; ++i)
+    {
+        if((i % num_of_columns) == 0)
+            out_pos += snprintf(log_buffer + out_pos, sizeof(log_buffer) - out_pos, "0x%04zx:", i);
+
+        out_pos += snprintf(log_buffer + out_pos, sizeof(log_buffer) - out_pos, " 0x%02x", buffer[i]);
+
+        if((i % num_of_columns) == num_of_columns - 1)
+        {
+            msg_info("%s", log_buffer);
+            out_pos = 0;
+        }
+    }
+
+    if(out_pos > 0)
+        msg_info("%s", log_buffer);
+}
---
+++ src/hexdump.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
+ *
+ * StrBoWare is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * StrBoWare is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StrBoWare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HEXDUMP_H
+#define HEXDUMP_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hexdump_to_log(const uint8_t *const buffer, size_t buffer_length,
+                    const char *what);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !HEXDUMP_H */

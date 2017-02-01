7ddf9df604777c561a2866560ac24e32f9b8d43c
Robert Tiemann <r.tiemann@ta-hifi.com>
2017-02-01 15:06:37 +0100

    New header file just for Cppcheck.
    Use --include=cppcheck.h to replace all those -D and -U options.
    
    Change-Id: I9114c78d0e41a567107e78b60342bf70224a3325

---
+++ cppcheck.h
@@ -0,0 +1,31 @@
+#undef _TYPE_size_t
+#undef _TYPE_wchar_t
+#undef _TYPE_ptrdiff_t
+#undef FILE
+#undef __BEOS__
+#undef __svr4__
+#undef _SCO_DS
+#undef __BSD_NET2__
+#undef ____386BSD____
+#undef __FreeBSD__
+#undef __NetBSD__
+#undef __BORLANDC__
+#undef WIN32
+#undef _AIX
+#undef __GCC_IEC_559
+#undef __GCC_IEC_559_COMPLEX
+#undef __ILP32__
+#undef HAVE_BOOST
+
+#define SYSLOG_NAMES
+#define HAVE_CONFIG_H 1
+#define NDEBUG
+#define _ALL_SOURCE 1
+#define _GNU_SOURCE 1
+#define _POSIX_PTHREAD_SEMANTICS 1
+#define _TANDEM_SOURCE 1
+#define __EXTENSIONS__ 1
+#define __x86_64__
+#define __GNUC__
+#define __linux__
+#define __WORDSIZE 64

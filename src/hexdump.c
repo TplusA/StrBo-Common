dfdd369b12782ee3e7fffccf0cec29db17c57f15
Robert Tiemann <r.tiemann@ta-hifi.com>
2017-09-08 15:05:21 +0200

    Output of hexdump_to_log() looks more like "hexdump -C".
    This commit changes the output format to the canonical hex+ASCII
    format which is commonly used all over the place.
    
    Change-Id: I57e9135f737cd9d259ded3ab67e25bd3504c6266

--- src/hexdump.c
+++ src/hexdump.c
@@ -21,8 +21,38 @@
 #endif /* HAVE_CONFIG_H */
 
 #include <stdio.h>
+#include <string.h>
 
 #include "hexdump.h"
+
+static size_t append_ascii(char *const log_buffer, size_t log_buffer_size,
+                           size_t out_pos, const uint8_t *const buffer,
+                           size_t start_pos, size_t end_pos,
+                           size_t missing_spaces)
+{
+    if(missing_spaces > 0)
+    {
+        memset(log_buffer + out_pos, ' ', missing_spaces);
+        out_pos += missing_spaces;
+    }
+
+    out_pos += snprintf(log_buffer + out_pos, log_buffer_size - out_pos, "  |");
+
+    for(size_t i = start_pos; i < end_pos; ++i)
+    {
+        const char ch = buffer[i];
+
+        if(ch >= 0x20 && ch < 0x7f)
+            log_buffer[out_pos++] = ch;
+        else
+            log_buffer[out_pos++] = '.';
+    }
+
+    log_buffer[out_pos++] = '|';
+    log_buffer[out_pos] = '\0';
+
+    return out_pos;
+}
 
 void hexdump_to_log(enum MessageVerboseLevel level,
                     const uint8_t *const buffer, size_t buffer_length,
@@ -37,24 +67,41 @@ void hexdump_to_log(enum MessageVerboseLevel level,
         return;
 
     static const size_t num_of_columns = 16;
+    static const size_t sep = 8;
     char log_buffer[1024];
     size_t out_pos = 0;
+    size_t start_i = 0;
+    size_t i;
 
-    for(size_t i = 0; i < buffer_length; ++i)
+    for(i = 0; i < buffer_length; ++i)
     {
         if((i % num_of_columns) == 0)
             out_pos += snprintf(log_buffer + out_pos, sizeof(log_buffer) - out_pos,
-                                "0x%04lx:", (unsigned long)i);
+                                "%04lx", (unsigned long)i);
 
-        out_pos += snprintf(log_buffer + out_pos, sizeof(log_buffer) - out_pos, " 0x%02x", buffer[i]);
+        if((i % sep) == 0)
+            log_buffer[out_pos++] = ' ';
+
+        out_pos += snprintf(log_buffer + out_pos, sizeof(log_buffer) - out_pos, " %02x", buffer[i]);
 
         if((i % num_of_columns) == num_of_columns - 1)
         {
+            out_pos = append_ascii(log_buffer, sizeof(log_buffer), out_pos,
+                                   buffer, start_i, i + 1, 0);
             msg_vinfo(level, "%s", log_buffer);
             out_pos = 0;
+            start_i = i + 1;
         }
     }
 
     if(out_pos > 0)
+    {
+        const size_t missing_columns =
+            num_of_columns - (buffer_length - start_i);
+
+        append_ascii(log_buffer, sizeof(log_buffer), out_pos,
+                     buffer, start_i, buffer_length,
+                     3 * missing_columns + missing_columns / sep);
         msg_vinfo(level, "%s", log_buffer);
+    }
 }

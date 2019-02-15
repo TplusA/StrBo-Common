e137b58c1ccb95eef2c99bdfd5489af9d76fed3d
Robert Tiemann <r.tiemann@ta-hifi.com>
2019-02-15 14:38:55 +0100

    cppcheck: Adjust cppcheck.h; simplify suppressions.
    There are 3 more files now for C++ which define __cplusplus to the
    correct value according to the standard.
    
    Change-Id: Iae38600c16ad7187a4c488759c1286df93156c98

--- cppcheck.h
+++ cppcheck.h
@@ -9,12 +9,16 @@
 #undef ____386BSD____
 #undef __FreeBSD__
 #undef __NetBSD__
+#undef __bsdi__
+#undef __sequent__
 #undef __BORLANDC__
 #undef WIN32
+#undef WINNT
 #undef _AIX
 #undef __GCC_IEC_559
 #undef __GCC_IEC_559_COMPLEX
 #undef __ILP32__
+#undef _FORTIFY_SOURCE
 #undef HAVE_BOOST
 
 #define SYSLOG_NAMES
@@ -27,5 +31,7 @@
 #define __EXTENSIONS__ 1
 #define __x86_64__
 #define __GNUC__
+#define __STDC__
 #define __linux__
 #define __WORDSIZE 64
+#define HAVE_CONFIG_H

c9ba60a8e164561576b74e1b36c3ce555343e883
Robert Tiemann <r.tiemann@ta-hifi.com>
2020-01-24 12:54:59 +0100

    Unit tests: Add blunt Valgrind suppressions file, enable its use.
    Change-Id: I8396085e7b50977a85b9d86993250b0bfc9f1cda

--- tests_new/valgrind.sh
+++ tests_new/valgrind.sh
@@ -10,9 +10,10 @@ RET=0
 for t in $(find . -maxdepth 1 -executable -name 'test_*')
 do
     echo "Valgrind $t"
-    $VALGRIND \
-        --xml=yes --xml-file=${t}.valgrind.xml \
+    ${VALGRIND} \
+        --xml=yes --xml-file="${t}.valgrind.xml" \
         --leak-check=full --show-reachable=yes --error-limit=no \
+        ${VALGRIND_OPTIONS} \
         $t
     test $? -eq 0 || RET=1
 done

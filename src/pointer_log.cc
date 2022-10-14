b99c998d1123d718fab8be5eb3ebea14caac1c8e
Robert Tiemann <r.tiemann@ta-hifi.com>
2022-10-14 12:58:00 +0200

    pointerlog: Add optional (manual) logging of pointers.
    Change-Id: I7ecc23153b759e36b8009d3232ab5b73c5109595

---
+++ src/pointer_log.cc
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2022  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include "pointer_log.hh"
+#include "messages.h"
+
+#include <cstdio>
+
+static void fill_extra_info(std::array<char, 1024> &buffer, const char *annotation,
+                            const char *ctx, const char *file, const char *fn, int line)
+{
+    if(annotation == nullptr)
+        annotation = "";
+
+    if(ctx == nullptr && file == nullptr)
+    {
+        if(annotation[0] == '\0')
+            buffer[0] = '\0';
+        else
+            std::snprintf(buffer.data(), buffer.size(), "%s",annotation + 1);
+    }
+    else if(ctx != nullptr && file == nullptr)
+        std::snprintf(buffer.data(), buffer.size(),
+                      "\"%s\"%s", ctx, annotation);
+    else if(ctx == nullptr && file != nullptr)
+        std::snprintf(buffer.data(), buffer.size(),
+                      "%s @%s:%s(%d)", annotation, file, fn, line);
+    else
+        std::snprintf(buffer.data(), buffer.size(),
+                      "\"%s\"%s @%s:%s(%d)", ctx, annotation, file, fn, line);
+}
+
+static void do_show(const void *vp, const PointerLog::MapType &pointers,
+                    const PointerLog::MapType::const_iterator &it,
+                    const char *extra_info)
+{
+    if(it != pointers.end())
+        msg_info("PointerLog: [%s(%p) %s:%s(%d)] %s",
+                 std::get<0>(it->second).c_str(), it->first,
+                 std::get<1>(it->second).c_str(), std::get<2>(it->second).c_str(),
+                 std::get<3>(it->second), extra_info);
+    else
+        msg_info("PointerLog: [%p ***UNKNOWN***] %s", vp, extra_info);
+}
+
+static const char *basename(const char *n)
+{
+    if(n == nullptr)
+        return nullptr;
+
+    const char *result = n;
+    for(const char *it = n; *it != '\0'; ++it)
+        if(*it == '/')
+            result = it + 1;
+
+    return result;
+}
+
+void PointerLog::add_untyped(const void *vp, const char *name, const char *ctx,
+                             const char *file, const char *fn, int line)
+{
+    file = basename(file);
+
+    if(vp == nullptr)
+    {
+        BUG("Tried to add nullptr to PointerLog at %s:%s(%d)", file, fn, line);
+        return;
+    }
+
+    std::array<char, 1024> extra_info;
+    auto it(pointers_.find(vp));
+
+    if(it != pointers_.end())
+    {
+        fill_extra_info(extra_info, " ***REPLACED***", ctx, file, fn, line);
+        do_show(vp, pointers_, it, extra_info.data());
+        it->second = std::make_tuple(name, file, fn, line);
+    }
+    else
+    {
+        auto result(pointers_.emplace(vp, std::make_tuple(name, file, fn, line)));
+        fill_extra_info(extra_info, " ***NEW***", ctx, file, fn, line);
+        do_show(vp, pointers_, result.first, extra_info.data());
+    }
+}
+
+void PointerLog::remove_untyped(const void *vp, const char *ctx,
+                                const char *file, const char *fn, int line)
+{
+    file = basename(file);
+
+    if(vp == nullptr)
+    {
+        BUG("Tried to remove nullptr from PointerLog at %s:%s(%d)", file, fn, line);
+        return;
+    }
+
+    std::array<char, 1024> extra_info;
+    fill_extra_info(extra_info, " ***REMOVED***", ctx, file, fn, line);
+
+    auto it(pointers_.find(vp));
+    do_show(vp, pointers_, it, extra_info.data());
+
+    if(it != pointers_.end())
+        pointers_.erase(it);
+}
+
+void PointerLog::show_untyped(const void *vp, const char *ctx,
+                              const char *file, const char *fn, int line)
+{
+    if(vp == nullptr)
+        return;
+
+    std::array<char, 1024> extra_info;
+    fill_extra_info(extra_info, nullptr, ctx, basename(file), fn, line);
+    do_show(vp, pointers_, pointers_.find(vp), extra_info.data());
+}
+
+void PointerLog::show_all(const char *ctx, const char *file, const char *fn, int line)
+{
+    std::array<char, 1024> extra_info;
+    fill_extra_info(extra_info, nullptr, ctx, basename(file), fn, line);
+
+    for(auto it = pointers_.begin(); it != pointers_.end(); ++it)
+        do_show(it->first, pointers_, it, extra_info.data());
+}
+
+PointerLog &PointerLog::get_singleton()
+{
+    static PointerLog pointer_log_singleton;
+    return pointer_log_singleton;
+}
---
+++ src/pointer_log.hh
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2022  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+#ifndef POINTER_LOG_HH
+#define POINTER_LOG_HH
+
+#include <string>
+#include <memory>
+#include <tuple>
+#include <map>
+
+/*!
+ * Store are retrieve information about a pointer.
+ *
+ * This class is not meant for production code. Its purpose is to help writing
+ * traces for pointers to the log in order to track down certain problems.
+ *
+ * The most convenient use of this class is via the macros defined in this
+ * header file, such as #POINTER_LOG_ADD().
+ */
+class PointerLog
+{
+  public:
+    using MapType =
+        std::map<const void *, std::tuple<std::string, std::string, std::string, int>>;
+
+  private:
+    MapType pointers_;
+
+  public:
+    PointerLog(const PointerLog &) = delete;
+    PointerLog(PointerLog &&) = delete;
+    PointerLog &operator=(const PointerLog &) = delete;
+    PointerLog &operator=(PointerLog &&) = delete;
+    explicit PointerLog() = default;
+
+    static PointerLog &get_singleton();
+
+    template <typename P>
+    void add(const P *ptr, const char *name, const char *ctx,
+             const char *file, const char *fn, int line)
+    {
+        add_untyped(static_cast<const void *>(ptr), name, ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void add(const std::shared_ptr<P> &ptr, const char *name, const char *ctx,
+             const char *file, const char *fn, int line)
+    {
+        add(ptr.get(), name, ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void add(const std::unique_ptr<P> &ptr, const char *name, const char *ctx,
+             const char *file, const char *fn, int line)
+    {
+        add(ptr.get(), name, ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void remove(const P *ptr, const char *ctx, const char *file, const char *fn, int line)
+    {
+        remove_untyped(static_cast<const void *>(ptr), ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void remove(const std::shared_ptr<P> &ptr, const char *ctx,
+                const char *file, const char *fn, int line)
+    {
+        remove(ptr.get(), ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void remove(const std::unique_ptr<P> &ptr, const char *ctx,
+                const char *file, const char *fn, int line)
+    {
+        remove(ptr.get(), ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void show(const P *ptr, const char *ctx = nullptr,
+              const char *file = nullptr, const char *fn = nullptr, int line = -1)
+    {
+        show_untyped(static_cast<const void *>(ptr), ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void show(const std::shared_ptr<P> &ptr, const char *ctx = nullptr,
+              const char *file = nullptr, const char *fn = nullptr, int line = -1)
+    {
+        show(ptr.get(), ctx, file, fn, line);
+    }
+
+    template <typename P>
+    void show(const std::unique_ptr<P> &ptr, const char *ctx = nullptr,
+              const char *file = nullptr, const char *fn = nullptr, int line = -1)
+    {
+        show(ptr.get(), ctx, file, fn, line);
+    }
+
+    void show_all(const char *ctx = nullptr,
+                  const char *file = nullptr, const char *fn = nullptr, int line = -1);
+
+  private:
+    void add_untyped(const void *vp, const char *name, const char *ctx,
+                     const char *file, const char *fn, int line);
+    void remove_untyped(const void *vp, const char *ctx,
+                        const char *file, const char *fn, int line);
+    void show_untyped(const void *vp, const char *ctx,
+                      const char *file, const char *fn, int line);
+};
+
+#define POINTER_LOG_ADD(PTR, NAME) \
+    PointerLog::get_singleton().add((PTR), (NAME), nullptr, __FILE__, __func__, __LINE__)
+
+#define POINTER_LOG_ADD_WITH_CTX(PTR, NAME, CTX) \
+    PointerLog::get_singleton().add((PTR), (NAME), (CTX), __FILE__, __func__, __LINE__)
+
+#define POINTER_LOG_REMOVE(PTR) \
+    PointerLog::get_singleton().remove((PTR), nullptr, __FILE__, __func__, __LINE__)
+
+#define POINTER_LOG_REMOVE_WITH_CTX(PTR, CTX) \
+    PointerLog::get_singleton().remove((PTR), (CTX), __FILE__, __func__, __LINE__)
+
+#define POINTER_LOG_SHOW(PTR) \
+    PointerLog::get_singleton().show((PTR), nullptr, __FILE__, __func__, __LINE__)
+
+#define POINTER_LOG_SHOW_WITH_CTX(PTR, CTX) \
+    PointerLog::get_singleton().show((PTR), (CTX), __FILE__, __func__, __LINE__)
+
+#endif /* !POINTER_LOG_HH */

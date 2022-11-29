2078d0575995a176913eb2faf7f91304aed28753
Robert Tiemann <r.tiemann@ta-hifi.com>
2022-11-29 12:04:17 +0100

    mock_timebase: Add mock for Timebase class.
    This is a mock which does not support expectations. Instead, it
    behaves like a clock which can be read out and whose time can be
    changed at will.
    
    Change-Id: I7d55d547fe803120a5ee1c9daa946ea5ad83dbad

---
+++ tests_new/mock_timebase.hh
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2022  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of the T+A Streaming Board software stack ("StrBoWare").
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+#ifndef MOCK_TIMEBASE_HH
+#define MOCK_TIMEBASE_HH
+
+#include "timebase.hh"
+#include "mock_expectation.hh"
+
+namespace MockTimebase
+{
+
+class Mock: public Timebase
+{
+  private:
+    time_point now_;
+    std::chrono::milliseconds auto_increment_;
+
+  public:
+    Mock(const Mock &) = delete;
+    Mock &operator=(const Mock &) = delete;
+
+    explicit Mock():
+        now_(time_point(std::chrono::milliseconds::zero())),
+        auto_increment_(std::chrono::milliseconds::zero())
+    {}
+
+    virtual ~Mock() {}
+
+    time_point now() const override
+    {
+        const_cast<Mock *>(this)->now_ += auto_increment_;
+        return now_;
+    }
+
+    void reset()
+    {
+        now_ = time_point(std::chrono::milliseconds::zero());
+        auto_increment_ = std::chrono::milliseconds::zero();
+    }
+
+    void set_clock(time_point &&now) { now_ = std::move(now); }
+
+    void step(unsigned int ms = 1)
+    {
+        now_ += std::chrono::milliseconds(ms);
+    }
+
+    void set_auto_increment(unsigned int ms = 1)
+    {
+        auto_increment_ = std::chrono::milliseconds(ms);
+    }
+};
+
+}
+
+#endif /* !MOCK_TIMEBASE_HH */
+
